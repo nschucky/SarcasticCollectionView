77484457b92a084e56b4b70ff8e9f5de0f60b3b8
Antonio Alves <antonioalvesdev@gmail.com>
2021-05-05 21:49:43 -0700

    feat: add section title and description

--- SarcasticCollectionView/IronicCell.swift
+++ SarcasticCollectionView/IronicCell.swift
@@ -12,36 +12,57 @@ class IronicCell: UICollectionViewCell {
     static let identifier = String(describing: IronicCell.self)
     
     let containerView = UIView()
+    
     let contentTitleLabel: UILabel = {
         let label = UILabel()
         label.backgroundColor = .clear
-        label.textColor = .white
-        label.font = UIFont(name: "Roboto-Bold", size: 32)
+        label.textColor = .black
+        label.font = UIFont(name: "Copperplate", size: 24)
+        label.textAlignment = .left
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.7
+        return label
+    }()
+    
+    let contentDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.textColor = .black
+        label.font = UIFont(name: "Chalkduster", size: 14)
         label.textAlignment = .left
         label.numberOfLines = 0
         return label
     }()
     
+    let stackView = UIStackView()
+    
     override init(frame: CGRect) {
-        super.init(frame: .zero)
+        super.init(frame: frame)
         
         addSubview(containerView)
-        containerView.layer.cornerRadius = 10
         containerView.translatesAutoresizingMaskIntoConstraints = false
+
         containerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        //containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
         containerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         containerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        containerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 40).isActive = true
+
+        stackView.addArrangedSubview(contentTitleLabel)
+        stackView.addArrangedSubview(contentDescriptionLabel)
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
         
-        containerView.addSubview(contentTitleLabel)
-        contentTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        contentTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
-        contentTitleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20).isActive = true
-        contentTitleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20).isActive = true
-        contentTitleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20).isActive = true
+        containerView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
         
-        //containerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
-        containerView.clipsToBounds = true
     }
     
     
@@ -52,10 +73,12 @@ class IronicCell: UICollectionViewCell {
     }
     
     func setup(with content: Content) {
-        containerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
 
-        containerView.backgroundColor = content.backgroundColor
-        contentTitleLabel.text = content.text
+        containerView.backgroundColor = UIColor(red: 0.93, green: 0.81, blue: 0.68, alpha: 1.00)
+        contentTitleLabel.textColor = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
+        contentDescriptionLabel.textColor = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.00)
+        contentTitleLabel.text = "A Section"
+        contentDescriptionLabel.text = content.text
     }
     
     override func layoutSubviews() {
--- SarcasticCollectionView/StrategyViewController.swift
+++ SarcasticCollectionView/StrategyViewController.swift
@@ -13,66 +13,28 @@ struct Content {
     let backgroundColor: UIColor
 }
 
-class ViewController: UICollectionViewController {
+class StrategyViewController: UICollectionViewController {
     
-    let data = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor."]
-    
-    var layout: UICollectionViewFlowLayout = {
-        let layout = UICollectionViewFlowLayout()
-        let width = UIScreen.main.bounds.size.width
-        layout.estimatedItemSize = CGSize(width: width, height: 10)
-        return layout
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        collectionView.register(IronicCell.self, forCellWithReuseIdentifier: IronicCell.identifier)
-        collectionView.collectionViewLayout = layout
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! MyCell
-        cell.label.text = data[indexPath.item]
-        return cell
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
-        super.traitCollectionDidChange(previousTraitCollection)
-    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
-        layout.invalidateLayout()
-        super.viewWillTransition(to: size, with: coordinator)
-    }
-}
-
-class StrategyViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    let collectionView: UICollectionView
     var content: [Content] = []
     
+    let layout: UICollectionViewFlowLayout
     init() {
         let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        layout.scrollDirection = .vertical
         layout.minimumLineSpacing = 20
-        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        super.init(nibName: nil, bundle: nil)
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        self.layout = layout
+        super.init(collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -80,34 +42,31 @@ class StrategyViewController: UIViewController, UICollectionViewDelegate, UIColl
         content.append(howContent)
         content.append(Content(text: "Smaller huh?", backgroundColor: .systemRed))
         content.append(Content(text: "just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, just a few lines, ", backgroundColor: .systemPurple))
-        
-        setupCollectionView()
+
+        collectionView.backgroundColor = UIColor(red: 0.93, green: 0.81, blue: 0.68, alpha: 1.00)
+        collectionView.register(IronicCell.self, forCellWithReuseIdentifier: IronicCell.identifier)
     }
-    
-    private func setupCollectionView() {
-        view.backgroundColor = .white
-        
-        view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1).isActive = true
-        collectionView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
-        collectionView.backgroundColor = .white
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        
-        
-        
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return content.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IronicCell.identifier, for: indexPath) as! IronicCell
         cell.setup(with: content[indexPath.item])
         return cell
     }
+    
+//    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+//        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
+//        super.traitCollectionDidChange(previousTraitCollection)
+//    }
+//    
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        layout.estimatedItemSize = CGSize(width: view.bounds.size.width, height: 10)
+//        layout.invalidateLayout()
+//        super.viewWillTransition(to: size, with: coordinator)
+//    }
 }
 
 
